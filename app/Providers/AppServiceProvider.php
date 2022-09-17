f783f9c40db52d55287783d68c5ea3e021530db3
HalqatWasl <halqatwasl.ye@gmail.com>
2022-09-17 19:02:50 +0300

    addh1
    kjkljk

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $dataUrl =[
+            'open works'=>'الاعمال المفتوحة',
+            'd'=>'المحافظة',
+            'd1'=>'صنعاء',
+            'profile'=>'الملف الشخصي',
+             'settings'=>'الاعدادات',
+             'worker' =>'العمال',
+
+
+        ];
+       view()->share('currentPath',explode('/',substr_replace(request()->path(),'',0,0)));
+       view()->share('dataUrl',$dataUrl);
+
     }
 }
