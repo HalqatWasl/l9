41ed0040179bd60719c7ade92e9b8d8868600c39
HalqatWasl <halqatwasl.ye@gmail.com>
2022-09-18 22:34:52 +0300

    t1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,13 +18,28 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        \App\Models\User::factory()->create([
-            'name' => 'admin',
-            'email' => 'admin@halqatwasl.com',
-            'email_verified_at' => now(),
-            'password' => Hash::make('admin@halqatwasl'),
-            'username' => Str::random(10) . '-' .uniqid(),
-            'is_admin' => '1',
+        // \App\Models\User::factory()->create([
+        //     'name' => 'admin',
+        //     'email' => 'admin@halqatwasl.com',
+        //     'email_verified_at' => now(),
+        //     'password' => Hash::make('admin@halqatwasl'),
+        //     'username' => Str::random(10) . '-' .uniqid(),
+        //     'is_admin' => '1',
+        // ]);
+
+        \App\Models\Departement::factory()->create([
+            'name' => 'نجار',
+            'is_active' => 1,
+        ]);
+
+        \App\Models\Province::factory()->create([
+            'name' => 'صنعاء',
+            'is_active' => 1,
+        ]);
+
+        \App\Models\Departement::factory()->create([
+            'name' => 'نجار',
+            'is_active' => 1,
         ]);
     }
 }
