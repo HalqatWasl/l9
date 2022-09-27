1a0e3eb2dddaa5b4be02d2823e46e83334546566
HalqatWasl <halqatwasl.ye@gmail.com>
2022-09-28 00:24:16 +0300

    Revert "add api Open_work"
    This reverts commit 267ee0ac47e75597f230bbe13e86e7a01cee6c3c.

--- app/Http/Controllers/AppController/OpenWorkController.php
+++ app/Http/Controllers/AppController/OpenWorkController.php
@@ -13,21 +13,11 @@ class OpenWorkController extends Controller
     //to show openWork
 
     public function index(){
+       
+  $openwork=Open_work::all()->where('stage','=',1);
 
-        $openwork=Open_work::join('users', 'users.id', '=', 'open_works.user_id')
-                           ->join('departements','departements.id','=','open_works.departement_id')
-                           ->join('provinces','provinces.id','=','open_works.province_id')
-                           ->join('directorates','directorates.id','=','open_works.directorate_id')
-                           ->where('open_works.stage','=',1)
-                           ->get(['open_works.*',
-                                  'users.name',
-                                  'users.username',
-                                  'departements.name as dep-name',
-                                  'provinces.name as pro-name',
-                                  'directorates.name as dir-name'
-                                ]);
-
-        return response(['Openwork'=>$openwork],200);
+ 
+return response(['Openwork'=>$openwork],200);
 }
 
 
@@ -38,7 +28,7 @@ class OpenWorkController extends Controller
 //to add openWork
      public function save(Request $request){
 
-
+     
              Open_work::create([
                  'user_id'        => $request->user_id,
                  'departement_id' => $request->departement_id,
@@ -51,14 +41,14 @@ class OpenWorkController extends Controller
                  'address'        => $request->address,
                  'stage'          => $request->stage,
                  'is_active'      => 1,
-
+    
              ]);
              return response([
                 'status'=>  'success'
                ], 200);
-
+           
                // return response('success'=>'تم اضافة العمل الى  معرض الاعمال');
-
+            
         }
-
+    
 }
